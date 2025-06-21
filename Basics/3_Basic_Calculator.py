90e4af310fa4661af9e25ba87d6d32f8a06649aa
Jahnvi Suthar <jahnvisuthar7@gmsil.com>
2025-06-21 17:48:51 +0530

    Resolved error

--- Basics/3_Basic_Calculator.py
+++ Basics/3_Basic_Calculator.py
@@ -1,25 +1,33 @@
-n1 = input("Enter number : ")
-n2 = input("Enter another number : ")
-o = input("Enter operation (+, -, *, /) : ")
+n1 = float(input("Enter number : "))
+n2 = float(input("Enter another number : "))
+o = input("Enter operation (+, -, *, /, %) : ")
 
-# Convert inputs to float
-num1 = float(n1)
-num2 = float(n2)
+# Keep asking for valid operator
+vo = ['+', '-', '*', '/', '%']
+o = input("Enter operation (+, -, *, /, %) : ")
+while o not in vo:
+    print("Invalid operation. Please enter one of +, -, *, /, %.")
+    o = input("Enter operation again: ")
 
 # Perform the operation
 if o == '+':
-    r = num1 + num2
+    r = n1 + n2
 elif o == '-':
-    r = num1 - num2
+    r = n1 - n2
 elif o == '*':
-    r = num1 * num2
+    r = n1 * n2
 elif o == '/':
-    if num2 != 0:
-        r = num1 / num2
+    if n2 != 0:
+        r = n1 / n2
     else:
         r = "INFINITY!"
+elif o == '%':
+    if n2 != 0:
+        r = n1 % n2
+    else:
+        r = "Cannot perform modulo by zero!"
 else:
-    r = "Invalid operation. Please enter one of +, -, *, /."
+    r = "Invalid operation. Please enter one of +, -, *, /, or %."
 
 print("Your result is :", r)
 # This code is a basic calculator that performs addition, subtraction, 
